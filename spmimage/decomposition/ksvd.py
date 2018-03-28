63c6543fa2dd77d58ab8046ec80845a0c966db52
tksmd <someda@isenshi.com>
2018-03-28 21:23:34 +0900

    apply code formatting #6

--- spmimage/decomposition/ksvd.py
+++ spmimage/decomposition/ksvd.py
@@ -45,177 +45,179 @@ Returns:
 		Number of iterations run. Returned only if `return_n_iter` is
 		set to True.
 """
-def ksvd(Y: np.ndarray, n_components: int, k0: int, tol: float, max_iter: int, code_init=None, dict_init=None, random_state=None, n_jobs=1):
-	if code_init is None:
-		A = Y[:, :n_components]
-	else:
-		A = code_init
-	A = np.dot(A, np.diag(1. / np.sqrt(np.diag(np.dot(A.T, A)))))
 
-	if dict_init is None:
-		X = np.zeros((A.shape[1], Y.shape[1]))
-	else:
-		X = dict_init
 
-	errors = [np.linalg.norm(Y-A.dot(X), 'fro')]
-	for k in range(max_iter):
-		X = sparse_encode(Y.T, A.T, algorithm='omp', n_nonzero_coefs=k0, n_jobs=n_jobs).T
+def ksvd(Y: np.ndarray, n_components: int, k0: int, tol: float, max_iter: int, code_init=None, dict_init=None,
+         random_state=None, n_jobs=1):
+    if code_init is None:
+        A = Y[:, :n_components]
+    else:
+        A = code_init
+    A = np.dot(A, np.diag(1. / np.sqrt(np.diag(np.dot(A.T, A)))))
 
-		for j in range(n_components):
-			x = X[j, :] != 0
-			if np.sum(x) == 0:
-				continue
-			X[j, x] = 0
+    if dict_init is None:
+        X = np.zeros((A.shape[1], Y.shape[1]))
+    else:
+        X = dict_init
 
-			error = Y[:, x] - np.dot(A, X[:, x])	
-			U, s, V = np.linalg.svd(error)
-			A[:, j] = U[:, 0]
-			X[j, x] = s[0] * V.T[:, 0]
+    errors = [np.linalg.norm(Y - A.dot(X), 'fro')]
+    for k in range(max_iter):
+        X = sparse_encode(Y.T, A.T, algorithm='omp', n_nonzero_coefs=k0, n_jobs=n_jobs).T
 
-		errors.append(np.linalg.norm(error, 'fro'))
-		if np.abs(errors[-1] - errors[-2]) < tol:
-			break
+        for j in range(n_components):
+            x = X[j, :] != 0
+            if np.sum(x) == 0:
+                continue
+            X[j, x] = 0
 
-	return A, X, errors, k
+            error = Y[:, x] - np.dot(A, X[:, x])
+            U, s, V = np.linalg.svd(error)
+            A[:, j] = U[:, 0]
+            X[j, x] = s[0] * V.T[:, 0]
+
+        errors.append(np.linalg.norm(error, 'fro'))
+        if np.abs(errors[-1] - errors[-2]) < tol:
+            break
+
+    return A, X, errors, k
 
 
 class Ksvd(BaseEstimator, SparseCodingMixin):
-	""" K-svd
-	Finds a dictionary that can be used to represent data using a sparse code.
-	Solves the optimization problem:
-		argmin \sum_{i=1}^M || y_i - Ax_i ||_2^2 such that ||x_i||_0 <= k_0 for all 1 <= i <= M
-		(A,{x_i}_{i=1}^M)
+    """ K-svd
+    Finds a dictionary that can be used to represent data using a sparse code.
+    Solves the optimization problem:
+        argmin \sum_{i=1}^M || y_i - Ax_i ||_2^2 such that ||x_i||_0 <= k_0 for all 1 <= i <= M
+        (A,{x_i}_{i=1}^M)
 
-	Parameters
-	----------
-		n_components : int,
-			number of dictionary elements to extract
-		k0 : int,
-			number of non-zero elements of sparse coding
-		max_iter : int,
-			maximum number of iterations to perform
-		tol : float,
-			tolerance for numerical error
-		transform_algorithm : {'lasso_lars', 'lasso_cd', 'lars', 'omp', 'threshold'}
-			Algorithm used to transform the data
-			lars: uses the least angle regression method (linear_model.lars_path)
-			lasso_lars: uses Lars to compute the Lasso solution
-			lasso_cd: uses the coordinate descent method to compute the
-			Lasso solution (linear_model.Lasso). lasso_lars will be faster if
-			the estimated components are sparse.
-			omp: uses orthogonal matching pursuit to estimate the sparse solution
-			threshold: squashes to zero all coefficients less than alpha from
-			the projection ``dictionary * X'``
-			.. versionadded:: 0.17
-			   *lasso_cd* coordinate descent method to improve speed.
-		transform_n_nonzero_coefs : int, ``0.1 * n_features`` by default
-			Number of nonzero coefficients to target in each column of the
-			solution. This is only used by `algorithm='lars'` and `algorithm='omp'`
-			and is overridden by `alpha` in the `omp` case.
-		transform_alpha : float, 1. by default
-			If `algorithm='lasso_lars'` or `algorithm='lasso_cd'`, `alpha` is the
-			penalty applied to the L1 norm.
-			If `algorithm='threshold'`, `alpha` is the absolute value of the
-			threshold below which coefficients will be squashed to zero.
-			If `algorithm='omp'`, `alpha` is the tolerance parameter: the value of
-			the reconstruction error targeted. In this case, it overrides
-			`n_nonzero_coefs`.
-		n_jobs : int,
-			number of parallel jobs to run
-		split_sign : bool, False by default
-			Whether to split the sparse feature vector into the concatenation of
-			its negative part and its positive part. This can improve the
-			performance of downstream classifiers.
-		random_state : int, RandomState instance or None, optional (default=None)
-			If int, random_state is the seed used by the random number generator;
-			If RandomState instance, random_state is the random number generator;
-			If None, the random number generator is the RandomState instance used
-			by `np.random`.
+    Parameters
+    ----------
+        n_components : int,
+            number of dictionary elements to extract
+        k0 : int,
+            number of non-zero elements of sparse coding
+        max_iter : int,
+            maximum number of iterations to perform
+        tol : float,
+            tolerance for numerical error
+        transform_algorithm : {'lasso_lars', 'lasso_cd', 'lars', 'omp', 'threshold'}
+            Algorithm used to transform the data
+            lars: uses the least angle regression method (linear_model.lars_path)
+            lasso_lars: uses Lars to compute the Lasso solution
+            lasso_cd: uses the coordinate descent method to compute the
+            Lasso solution (linear_model.Lasso). lasso_lars will be faster if
+            the estimated components are sparse.
+            omp: uses orthogonal matching pursuit to estimate the sparse solution
+            threshold: squashes to zero all coefficients less than alpha from
+            the projection ``dictionary * X'``
+            .. versionadded:: 0.17
+               *lasso_cd* coordinate descent method to improve speed.
+        transform_n_nonzero_coefs : int, ``0.1 * n_features`` by default
+            Number of nonzero coefficients to target in each column of the
+            solution. This is only used by `algorithm='lars'` and `algorithm='omp'`
+            and is overridden by `alpha` in the `omp` case.
+        transform_alpha : float, 1. by default
+            If `algorithm='lasso_lars'` or `algorithm='lasso_cd'`, `alpha` is the
+            penalty applied to the L1 norm.
+            If `algorithm='threshold'`, `alpha` is the absolute value of the
+            threshold below which coefficients will be squashed to zero.
+            If `algorithm='omp'`, `alpha` is the tolerance parameter: the value of
+            the reconstruction error targeted. In this case, it overrides
+            `n_nonzero_coefs`.
+        n_jobs : int,
+            number of parallel jobs to run
+        split_sign : bool, False by default
+            Whether to split the sparse feature vector into the concatenation of
+            its negative part and its positive part. This can improve the
+            performance of downstream classifiers.
+        random_state : int, RandomState instance or None, optional (default=None)
+            If int, random_state is the seed used by the random number generator;
+            If RandomState instance, random_state is the random number generator;
+            If None, the random number generator is the RandomState instance used
+            by `np.random`.
 
-	Attributes
-	----------
-		components_ : array, [n_components, n_features]
-			dictionary atoms extracted from the data
-		error_ : array
-			vector of errors at each iteration
-		n_iter_ : int
-			Number of iterations run.
-	**References:**
-		Elad, Michael, and Michal Aharon. 
-		"Image denoising via sparse and redundant representations over learned dictionaries." 
-		IEEE Transactions on Image processing 15.12 (2006): 3736-3745.
-	----------
+    Attributes
+    ----------
+        components_ : array, [n_components, n_features]
+            dictionary atoms extracted from the data
+        error_ : array
+            vector of errors at each iteration
+        n_iter_ : int
+            Number of iterations run.
+    **References:**
+        Elad, Michael, and Michal Aharon.
+        "Image denoising via sparse and redundant representations over learned dictionaries."
+        IEEE Transactions on Image processing 15.12 (2006): 3736-3745.
+    ----------
 
-	"""
+    """
 
-	def __init__(self, n_components=None, k0=None, max_iter=100, tol=1e-8,
-				 transform_algorithm='omp', transform_n_nonzero_coefs=None,
-				 transform_alpha=None, n_jobs=1, 
-				 split_sign=False, random_state=None):
-		self._set_sparse_coding_params(n_components, transform_algorithm,
-									   transform_n_nonzero_coefs,
-									   transform_alpha, split_sign, n_jobs)
-		self.k0 = k0
-		self.max_iter = max_iter
-		self.tol = tol
-		self.random_state = random_state
+    def __init__(self, n_components=None, k0=None, max_iter=100, tol=1e-8,
+                 transform_algorithm='omp', transform_n_nonzero_coefs=None,
+                 transform_alpha=None, n_jobs=1,
+                 split_sign=False, random_state=None):
+        self._set_sparse_coding_params(n_components, transform_algorithm,
+                                       transform_n_nonzero_coefs,
+                                       transform_alpha, split_sign, n_jobs)
+        self.k0 = k0
+        self.max_iter = max_iter
+        self.tol = tol
+        self.random_state = random_state
 
+    def fit(self, X, y=None):
+        """Fit the model from data in X.
+        Parameters
+        ----------
+        X : array-like, shape (n_samples, n_features)
+            Training vector, where n_samples in the number of samples
+            and n_features is the number of features.
+        y : Ignored
+        Returns
+        -------
+        self : object
+            Returns the object itself
+        """
 
-	def fit(self, X, y=None):
-		"""Fit the model from data in X.
-		Parameters
-		----------
-		X : array-like, shape (n_samples, n_features)
-			Training vector, where n_samples in the number of samples
-			and n_features is the number of features.
-		y : Ignored
-		Returns
-		-------
-		self : object
-			Returns the object itself
-		"""
+        # Turn seed into a np.random.RandomState instance
+        random_state = check_random_state(self.random_state)
 
-		#Turn seed into a np.random.RandomState instance
-		random_state = check_random_state(self.random_state)
+        # Input validation on an array, list, sparse matrix or similar.
+        # By default, the input is converted to an at least 2D numpy array. If the dtype of the array is object, attempt converting to float, raising on failure.
+        X = check_array(X)
+        n_samples, n_features = X.shape
+        if self.n_components is None:
+            n_components = X.shape[1]
+        else:
+            n_components = self.n_components
 
-		#Input validation on an array, list, sparse matrix or similar.
-		#By default, the input is converted to an at least 2D numpy array. If the dtype of the array is object, attempt converting to float, raising on failure.
-		X = check_array(X)
-		n_samples, n_features = X.shape
-		if self.n_components is None:
-			n_components = X.shape[1]
-		else:
-			n_components = self.n_components
+        if self.k0 is None:
+            k0 = n_features
+        elif self.k0 > n_features:
+            k0 = n_features
+        else:
+            k0 = self.k0
 
-		if self.k0 is None:
-			k0 = n_features
-		elif self.k0 > n_features:
-			k0 = n_features
-		else:
-			k0 = self.k0
+        # initialize code
+        code_init = random_state.rand(n_samples, n_components)
+        # initialize dictionary
+        dict_init = random_state.rand(n_components, n_features)
 
-		#initialize code
-		code_init = random_state.rand(n_samples,n_components)
-		#initialize dictionary
-		dict_init = random_state.rand(n_components, n_features)
+        self.components, code, self.errors, self.n_iter = ksvd(
+            X, n_components, k0,
+            tol=self.tol, max_iter=self.max_iter,
+            code_init=code_init,
+            dict_init=dict_init,
+            random_state=random_state,
+            n_jobs=self.n_jobs)
 
-		self.components, code, self.errors, self.n_iter = ksvd(
-			X, n_components, k0,
-			tol=self.tol, max_iter=self.max_iter,
-			code_init=code_init,
-			dict_init=dict_init,
-			random_state=random_state,
-			n_jobs=self.n_jobs)
+        return self
 
-		return self
 
 def main():
-	X = np.random.rand(50,100)
-	model = Ksvd(n_components=20, k0=10, n_jobs=1)
-	model.fit(X)
-	print(model.components)
+    X = np.random.rand(50, 100)
+    model = Ksvd(n_components=20, k0=10, n_jobs=1)
+    model.fit(X)
+    print(model.components)
 
 
 if __name__ == '__main__':
-	main()
-
+    main()
