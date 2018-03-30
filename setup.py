1390db44a8685b815f6cbb834430e2480c0dc17d
tksmd <someda@isenshi.com>
2018-03-30 15:30:04 +0900

    improve setup.py #3

--- setup.py
+++ setup.py
@@ -1,4 +1,21 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+import sys
+
+LICENSE = 'Modified BSD'
+
+if not (3, 5) <= sys.version_info[:2]:
+    raise Exception("spm-image requires a Python 3 version newer than 3.5. \n now running on %s" % sys.version)
+
+# load basic information from files
+with Path('spmimage', '__init__.py').open() as f:
+    for line in f:
+        if line.startswith('__version__'):
+            VERSION = line.strip().split()[-1][1:-1]
+            break
+
+with Path('requirements.txt').open() as f:
+    INSTALL_REQUIRES = [line.strip() for line in f.readlines() if line]
 
 setup(
     name='spm-image',
@@ -6,7 +23,9 @@ setup(
     author_email='takashi@hacarus.com',
     url='https://github.com/hacarus/spm-image',
     description='Sparse modeling and Compressive sensing in Python',
-    version='0.0.2',
+    version=VERSION,
     packages=find_packages(),
-    test_suite='tests'
+    install_requires=INSTALL_REQUIRES,
+    test_suite='tests',
+    license=LICENSE
 )
