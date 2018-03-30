07423ac306f05687acbafaf494c5666048fc81e3
tksmd <someda@isenshi.com>
2018-03-30 15:03:08 +0900

    increate iteration in testing to reduce error #6

--- tests/test_decomposition_ksvd.py
+++ tests/test_decomposition_ksvd.py
@@ -8,7 +8,9 @@ import numpy as np
 
 class TestKSVD(TestCase):
 
-    def generate_input(self, n_samples: int, n_features: int, n_components: int, k0: int) -> Tuple[np.ndarray, np.ndarray]:
+    @staticmethod
+    def generate_input(n_samples: int, n_features: int, n_components: int, k0: int) \
+            -> Tuple[np.ndarray, np.ndarray]:
         # random dictionary base
         A0 = np.random.randn(n_components, n_features)
         A0 = np.dot(A0, np.diag(1. / np.sqrt(np.diag(np.dot(A0.T, A0)))))
@@ -25,20 +27,22 @@ class TestKSVD(TestCase):
         n_samples = 512
         n_features = 32
         n_components = 24
-        max_iter = 100
+        max_iter = 500
 
         A0, X = self.generate_input(n_samples, n_features, n_components, k0)
         model = KSVD(n_components=n_components, k0=k0, max_iter=max_iter)
         model.fit(X)
 
-        norm = np.linalg.norm(model.components_ - A0, ord='fro')
-
-        self.assertTrue(model.error_[-1] < 75)
-        self.assertTrue(norm < 50)
+        # check error of learning
+        self.assertTrue(model.error_[-1] < 10)
         self.assertTrue(model.n_iter_ <= max_iter)
 
+        # check estimated dictionary
+        norm = np.linalg.norm(model.components_ - A0, ord='fro')
+        self.assertTrue(norm < 15)
+
+        # check reconstructed data
         code = model.transform(X)
         reconstructed = np.dot(code, model.components_)
-        reconstruct_error =  np.linalg.norm(reconstructed - X, ord='fro')
-        print(reconstruct_error)
-
+        reconstruct_error = np.linalg.norm(reconstructed - X, ord='fro')
+        self.assertTrue(reconstruct_error < 15)
