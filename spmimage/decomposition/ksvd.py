4b0ad5f6415985c3babb66c392392186b26e75b5
masui <sutkss@gmail.com>
2018-04-02 14:33:36 +0900

    #8 implement masked ksvd

--- spmimage/decomposition/ksvd.py
+++ spmimage/decomposition/ksvd.py
@@ -10,7 +10,7 @@ logger = getLogger(__name__)
 
 
 def _ksvd(Y: np.ndarray, n_components: int, k0: int, max_iter: int, tol: float, code_init: np.ndarray = None,
-          dict_init: np.ndarray = None, n_jobs: int = 1):
+          dict_init: np.ndarray = None, mask: np.ndarray = None, n_jobs: int = 1):
     """_ksvd
     Finds a dictionary that can be used to represent data using a sparse code.
     Solves the optimization problem:
@@ -66,7 +66,13 @@ def _ksvd(Y: np.ndarray, n_components: int, k0: int, max_iter: int, tol: float, 
     errors = [np.linalg.norm(Y - W.dot(H), 'fro')]
     k = -1
     for k in range(max_iter):
-        W = sparse_encode(Y, H, algorithm='omp', n_nonzero_coefs=k0, n_jobs=n_jobs)
+        if mask is None:
+            W = sparse_encode(Y, H, algorithm='omp', n_nonzero_coefs=k0, n_jobs=n_jobs)
+        else:
+            for i in range(Y.shape[0]):
+                y_mask = Y[i, :][mask[i,:]==1].reshape(1,-1)
+                H_mask = H[:, mask[i,:]==1]
+                W[i, :] = sparse_encode(y_mask, H_mask, algorithm='omp', n_nonzero_coefs=k0, n_jobs=n_jobs)
 
         for j in range(n_components):
             x = W[:, j] != 0
@@ -104,6 +110,8 @@ class KSVD(BaseEstimator, SparseCodingMixin):
             maximum number of iterations to perform
         tol : float,
             tolerance for numerical error
+        missing_value : float,
+            missing value in the data
         transform_algorithm : {'lasso_lars', 'lasso_cd', 'lars', 'omp', 'threshold'}
             Algorithm used to transform the data
             lars: uses the least angle regression method (linear_model.lars_path)
@@ -157,7 +165,8 @@ class KSVD(BaseEstimator, SparseCodingMixin):
     """
 
     def __init__(self, n_components=None, k0=None, max_iter=1000, tol=1e-8,
-                 transform_algorithm='omp', transform_n_nonzero_coefs=None,
+                 missing_value=None,transform_algorithm='omp',
+                 transform_n_nonzero_coefs=None,
                  transform_alpha=None, n_jobs=1,
                  split_sign=False, random_state=None):
         self._set_sparse_coding_params(n_components, transform_algorithm,
@@ -166,6 +175,7 @@ class KSVD(BaseEstimator, SparseCodingMixin):
         self.k0 = k0
         self.max_iter = max_iter
         self.tol = tol
+        self.missing_value = missing_value
         self.random_state = random_state
 
     def fit(self, X, y=None):
@@ -194,6 +204,10 @@ class KSVD(BaseEstimator, SparseCodingMixin):
         else:
             n_components = self.n_components
 
+        mask = None
+        if not self.missing_value is None:
+            mask = np.where(X == self.missing_value, 1, 0)
+
         if self.k0 is None:
             k0 = n_features
         elif self.k0 > n_features:
@@ -210,6 +224,6 @@ class KSVD(BaseEstimator, SparseCodingMixin):
             X, n_components, k0,
             max_iter=self.max_iter, tol=self.tol,
             code_init=code_init, dict_init=dict_init,
-            n_jobs=self.n_jobs)
+            mask=mask, n_jobs=self.n_jobs)
 
         return self
