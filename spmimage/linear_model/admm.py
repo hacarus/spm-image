b638704db50742246f7a38f91ec2fbaa3a2349e0
tksmd <takashi@hacarus.com>
2018-04-03 12:15:33 +0900

    bolierplate for adding LassoADMM #16

---
+++ spmimage/linear_model/admm.py
@@ -0,0 +1,11 @@
+from sklearn.linear_model.base import LinearModel, RegressorMixin
+
+
+class LassoADMM(LinearModel, RegressorMixin):
+    def __init__(self):
+        # TODO
+        pass
+
+    def fit(self, X, y):
+        # TODO
+        pass
---
+++ tests/test_linear_model_admm.py
@@ -0,0 +1,13 @@
+import unittest
+
+from spmimage.linear_model import LassoADMM
+
+
+class TestLassoADMM(unittest.TestCase):
+    def test_lasso_admm(self):
+        # TODO
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
