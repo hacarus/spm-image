ee8b1f9242a68e207c73a6f9508786fa1b213f88
tksmd <someda@isenshi.com>
2018-04-09 08:48:06 +0900

    fix unresolved import and reformat some parts of admm.py #16

--- spmimage/linear_model/admm.py
+++ spmimage/linear_model/admm.py
@@ -2,11 +2,16 @@ from logging import getLogger
 
 import numpy as np
 
-from sklearn.utils import check_array
+from sklearn.utils import check_array, check_X_y
 from sklearn.base import RegressorMixin
-from sklearn.linear_model.base import LinearModel, _preprocess_data
+from sklearn.linear_model.base import LinearModel
 
 logger = getLogger(__name__)
+
+
+def _soft_threshold(X: np.ndarray, thresh: float) -> np.ndarray:
+    return np.where(np.abs(X) <= thresh, 0, X - thresh * np.sign(X))
+
 
 class LassoADMM(LinearModel, RegressorMixin):
     """Linear Model trained with L1 prior as regularizer (aka the Lasso)
@@ -32,9 +37,9 @@ class LassoADMM(LinearModel, RegressorMixin):
 
     def fit(self, X, y, check_input=False):
         if self.alpha == 0:
-            warnings.warn("With alpha=0, this algorithm does not converge "
-                          "well. You are advised to use the LinearRegression "
-                          "estimator", stacklevel=2)
+            logger.warning("""
+With alpha=0, this algorithm does not converge well. You are advised to use the LinearRegression estimator            
+""")
 
         if check_input:
             X, y = check_X_y(X, y, accept_sparse='csc',
@@ -44,10 +49,9 @@ class LassoADMM(LinearModel, RegressorMixin):
             y = check_array(y, order='F', copy=False, dtype=X.dtype.type,
                             ensure_2d=False)
 
-        X, y, X_offset, y_offset, X_scale = \
-            _preprocess_data(X, y, fit_intercept=self.fit_intercept,
-                             normalize=self.normalize,
-                             copy=self.copy_X and not check_input)
+        X, y, X_offset, y_offset, X_scale = self._preprocess_data(X, y, fit_intercept=self.fit_intercept,
+                                                                  normalize=self.normalize,
+                                                                  copy=self.copy_X and not check_input)
 
         if y.ndim == 1:
             y = y[:, np.newaxis]
@@ -62,11 +66,7 @@ class LassoADMM(LinearModel, RegressorMixin):
 
     def _cost_function(self, y, X, w):
         n_samples = X.shape[0]
-        return (np.linalg.norm(y - X.dot(w)) / n_samples +
-                self.alpha * np.sum(np.abs(w)))
-
-    def _soft_threshold(self, X: np.ndarray, thresh: float) -> np.ndarray:
-        return np.where(np.abs(X) <= thresh, 0, X - thresh * np.sign(X))
+        return np.linalg.norm(y - X.dot(w)) / n_samples + self.alpha * np.sum(np.abs(w))
 
     def _admm(self, X, y):
         n_samples, n_features = X.shape
@@ -78,8 +78,7 @@ class LassoADMM(LinearModel, RegressorMixin):
         h_t = np.zeros(w_t.shape)
 
         # Calculate inverse matrix
-        inv_matrix = np.linalg.inv(X.T.dot(X) / n_samples +
-                                   self.rho * np.eye(n_features))
+        inv_matrix = np.linalg.inv(X.T.dot(X) / n_samples + self.rho * np.eye(n_features))
 
         self.n_iter_ = []
         # Update ADMM parameters by columns
@@ -89,12 +88,8 @@ class LassoADMM(LinearModel, RegressorMixin):
                 gap = self._cost_function(y[:, k], X, w_t[:, k])
 
                 # Update
-                w_t[:, k] = \
-                    inv_matrix.dot(X.T.dot(y[:, k]) / n_samples +
-                                   (self.rho * z_t[:, k]) - h_t[:, k])
-                z_t[:, k] = \
-                    self._soft_threshold(w_t[:, k] + (h_t[:, k] / self.rho),
-                                         self.threshold)
+                w_t[:, k] = inv_matrix.dot(X.T.dot(y[:, k]) / n_samples + (self.rho * z_t[:, k]) - h_t[:, k])
+                z_t[:, k] = _soft_threshold(w_t[:, k] + (h_t[:, k] / self.rho), self.threshold)
                 h_t[:, k] += self.rho * (w_t[:, k] - z_t[:, k])
 
                 # after cost
