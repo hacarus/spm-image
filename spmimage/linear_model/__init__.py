d3ccd12d933f7480e3e12aa54a4ad627527be9fb
junpei <junpei@hacarus.com>
2019-12-13 14:14:44 +0900

    unittest #73

--- spmimage/linear_model/__init__.py
+++ spmimage/linear_model/__init__.py
@@ -1,8 +1,10 @@
 from .admm import LassoADMM, FusedLassoADMM, TrendFilteringADMM, QuadraticTrendFilteringADMM
+from .hmlasso import HMLasso
 
 __all__ = [
     'LassoADMM',
     'FusedLassoADMM',
     'TrendFilteringADMM',
     'QuadraticTrendFilteringADMM',
+    'HMLasso',
 ]
