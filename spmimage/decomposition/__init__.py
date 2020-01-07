5519b1ef4870bf5439fc4501394aa8296cda1146
masui <ryuji@hacarus.com>
2020-01-07 14:38:38 +0900

    #79 add generate dct dictionary function

--- spmimage/decomposition/__init__.py
+++ spmimage/decomposition/__init__.py
@@ -1,7 +1,9 @@
-from .ksvd import KSVD
+from .dct import generate_dct_dictionary
 from .dict_learning import sparse_encode_with_mask
+from .ksvd import KSVD
 
 __all__ = [
     'KSVD',
-    'sparse_encode_with_mask'
+    'sparse_encode_with_mask',
+    'generate_dct_dictionary'
 ]
